0ae884cc31ba2b9198fc7276cd5f23108a94cdf3
Heinrikii <henrikii@outlook.com>
2022-09-15 19:10:46 -0300

    Finalizado

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,44 @@
-public class App {
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO:Conhecer e importar a classe Scanner
+        Scanner input = new Scanner(System.in);
+        
+
+        //Exibir as mensagens para o nosso usuário
+        //Obter pela scanner os valores digitados no terminal
+
+        System.out.println("Por Favor digite o número da Conta: ");
+        int conta = input.nextInt();
+
+        System.out.println("Por favor digite o número da sua Agência: ");
+        String agencia = input.next();
+
+
+        System.out.println("Por favor digite o seu nome: ");
+        String nome = input.next();
+        
+       // System.out.println("Por favor digite o seu sobrenome: ");
+        //String sobreNome = input.next();
+
+
+        System.out.println("Informe o valor para deposito: ");
+        double saldo = input.nextDouble();
+
+        //Exibir a mensagem conta criada
+        
+        System.out.println("***********Comprovante de Abertura de conta******************");
+        System.out.println("*************************************************************");
+        System.out.println("**********||Nome: " + nome+"           \t\t||***********");
+        System.out.println("**********||Agência: " + agencia+"     \t\t\t||***********");
+        System.out.println("**********||Conta: " + conta+"         \t\t||***********");
+        System.out.println("**********||Saldo: R$" +saldo+" reais  \t\t||***********");
+        System.out.println("*************************************************************");
+        System.out.println("*************************************************************");
+        
+        
+        input.close();
     }
 }
